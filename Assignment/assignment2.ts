931d98affb413b3b79b673cf4d1c3d2c960cee2f
Madhushree13 <madhushree.ms13@gmail.com>
2026-02-14 00:22:29 +0530

    Update assignment2.ts

--- Assignment/assignment2.ts
+++ Assignment/assignment2.ts
@@ -3,7 +3,7 @@ If the credit score is above 750, the loan is automatically approved.
 If the credit score is between 650 and 750, additional checks are performed.
 If the credit score is below 650, the loan is denied.*/
 
-/*
+
 let credScore:number =751;
 if(credScore>750)
 {
@@ -20,7 +20,7 @@ else if(credScore>=650 && credScore<=750)
 /*Income:
 For credit scores between 650 and 750, the customer’s income must be at least $50,000
 for the loan to be considered.*/
-/*
+
 let credScoree:number =651;
 let income:number =50000;
 if(credScoree>=650 && credScoree<=750)
@@ -39,7 +39,7 @@ console.log("eligible for loan");
 o If the customer’s income is at least 50,000, the system checks whether the customer is
 employed.
 o If the customer is unemployed, the loan is denied.*/
-/*
+
 let custIncome:number =50000;
 
 if(custIncome>=50000)
@@ -54,7 +54,7 @@ if(custIncome>=50000)
 o If the customer is employed, the system checks the debt-to-income (DTI) ratio.
 o If the DTI ratio is less than 40%, the loan is approved.
 o If the DTI ratio is 40% or greater, the loan is denied.*/
-/*
+
 let dti:number =39
 let isEmployeed: boolean= true;
 if(isEmployeed==true)
